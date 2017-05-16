d16f980664c434ef66f0d693487f480ec3a15fef
DKnight5544 <dave@mostlyspam.com>
2017-05-16 11:31:54 -0700

    Change For Loops to forEach + Add Swapper Class

---
+++ FiftyTwoCards/js/CardSwapper.js
@@ -0,0 +1,14 @@
+﻿
+'use strict';
+
+function CardSwapper(card1, card2) {
+    let c1 = card1;
+    let c2 = card2;
+
+    this.swap = function () {
+        const props = c1.cloneProps();
+        c1.setProps(c2.cloneProps());
+        c2.setProps(props);
+    }
+
+}
