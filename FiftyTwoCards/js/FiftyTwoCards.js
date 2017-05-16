084b8313b85a6bf3f110265b9bf7d5e4aadc5590
David Knight <dave@mostlyspam.com>
2017-05-16 11:32:59 -0700

    Change For Loops to forEach + Add Swapper Class (#4)

--- FiftyTwoCards/js/FiftyTwoCards.js
+++ FiftyTwoCards/js/FiftyTwoCards.js
@@ -3,19 +3,20 @@
 
 const CARD_ARRAY = [];
 const SHUFFLE_ARRAY = [];
+const SUIT_ORDER_ARRAY = [0, 1, 3, 2];
+const CARD_VALUE_ARRAY = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
 function body_onload() {
     createCards();
 }
 
+
 function createCards() {
 
     const container = document.getElementById("mainContainer");
-    const suitValueArray = [0, 1, 3, 2];
 
-    for (let row = 0; row < 4; row++) {
-        let suit = suitValueArray[row];
-        for (let value = 0; value < 13; value++) {
+    SUIT_ORDER_ARRAY.forEach(function (suit) {
+        CARD_VALUE_ARRAY.forEach(function (value) {
 
             const borderColor = (suit === 0 || suit === 3) ? "black" : "red";
             const marginTop = (-2 - (suit * 123)).toString() + "px";
@@ -26,46 +27,44 @@ function createCards() {
             const card = new Card(props, container);
 
             CARD_ARRAY.push(card);
-        }
-    }
+
+        })
+    })
 }
 
 
 function reset() {
-    for (let index = 0; index < CARD_ARRAY.length ; index++) {
-        CARD_ARRAY[index].reset();
-    }
+    CARD_ARRAY.forEach(function (card) { card.reset() });
+    SHUFFLE_ARRAY.length = 0;
 }
 
+
 function shuffleCards() {
-    for (let index = CARD_ARRAY.length - 1; index > 0; index--) {
-        let swapIndex = Math.floor(Math.random() * index);
-        let card1 = CARD_ARRAY[index];
-        let card2 = CARD_ARRAY[swapIndex];
-        let tempProps = card1.cloneProps();
-        card1.setProps(card2.cloneProps());
-        card2.setProps(tempProps);
-    }
+    loadShuffleArray();
+    SHUFFLE_ARRAY.forEach(function (swapper) {
+        swapper.swap();
+    })
+    SHUFFLE_ARRAY.length = 0;
 }
 
 function visuallyShuffleCards() {
-    for (let index = CARD_ARRAY.length - 1; index > 0; index--) {
-        const map = new Object();
-        map.index1 = index;
-        map.index2 = Math.floor(Math.random() * index);
-        SHUFFLE_ARRAY.push(map);
-    }
-    if (SHUFFLE_ARRAY.length === 51) shuffleOneCard();
+    loadShuffleArray();
+    if (SHUFFLE_ARRAY.length === CARD_ARRAY.length) shuffleOneCard();
+}
+
+function loadShuffleArray() {
+    CARD_ARRAY.slice(0).reverse().forEach(function (card1, index) {
+        let swapIndex = Math.floor(Math.random() * (CARD_ARRAY.length - 1 - index));
+        let card2 = CARD_ARRAY[swapIndex];
+        let swapper = new CardSwapper(card1, card2);
+        SHUFFLE_ARRAY.push(swapper);
+    })
 }
 
 function shuffleOneCard() {
-    const map = SHUFFLE_ARRAY.shift();
-    if (map) {
-        const card1 = CARD_ARRAY[map.index1];
-        const card2 = CARD_ARRAY[map.index2];
-        const tempProps = card1.cloneProps();
-        card1.setProps(card2.cloneProps());
-        card2.setProps(tempProps);
+    const swapper = SHUFFLE_ARRAY.shift();
+    if (swapper) {
+        swapper.swap();
         setTimeout(shuffleOneCard, 200);
     }
 }
